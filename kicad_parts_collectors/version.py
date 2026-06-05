8a7964e2226e42fe8830852c1bc2f4cd928e15a8
jihoon_oh <jh.oh@hroboticsinc.com>
2026-06-05 11:53:31 +0900

    Bump version to 1.0.5

--- kicad_parts_collectors/version.py
+++ kicad_parts_collectors/version.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 
-APP_VERSION = "1.0.4"
+APP_VERSION = "1.0.5"
 GITHUB_OWNER = "murse2000"
 GITHUB_REPO = "KicadPartsCollectors"
 RELEASE_ASSET_NAME = "KiCadPartsCollector.exe"
